c6f117a680876b43c57fde843089944ce471587e
gifton <giftono@gmail.com>
2018-11-18 08:27:15 -0800

    added static date, line, pain, and fatigue labels

--- CareTake/CareTake/View/DashboardCell.swift
+++ CareTake/CareTake/View/DashboardCell.swift
@@ -17,7 +17,22 @@ class RecordCell: UICollectionViewCell {
         self.layer.cornerRadius = 10
     }
     
-    let date = UILabel()
+    var date = UILabel()
+    let line = UIView()
+    let painLabel = UILabel()
+    var pain = PainView(pain: 8, frame: CGRect(x: 20, y: 100, width: 100, height: 100))
+    var fatigue = UILabel()
+    var foodEaten = UILabel()
+    let symptomsCV: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let frame = CGRect(x: 0, y: ScreenSize.SCREEN_HEIGHT / 5.5, width: ScreenSize.SCREEN_WIDTH , height: 108)
+        layout.scrollDirection = .horizontal
+        let cv = UICollectionView(frame: frame, collectionViewLayout: layout)
+        cv.backgroundColor = UIColor(red:0.91, green:0.91, blue:0.91, alpha:1.0)
+        cv.isSpringLoaded = true
+        cv.showsHorizontalScrollIndicator = false
+        return cv
+    }()
     
     
     required init?(coder aDecoder: NSCoder) {
@@ -26,11 +41,38 @@ class RecordCell: UICollectionViewCell {
     
     func setupViews() {
         addSubview(date)
+        addSubview(line)
+        addSubview(painLabel)
+        addSubview(pain)
+        addSubview(fatigue)
+        addSubview(foodEaten)
+        
         date.translatesAutoresizingMaskIntoConstraints = false
-        date.topAnchor.constraint(equalTo: topAnchor, constant: 24).isActive = true
+        date.topAnchor.constraint(equalTo: topAnchor, constant: 20).isActive = true
         date.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20).isActive = true
-        date.font = UIFont(name: "HelveticaNeue-light", size: fontSize.xXLarge.rawValue)
+        
+        line.translatesAutoresizingMaskIntoConstraints = false
+        line.topAnchor.constraint(equalTo: date.bottomAnchor, constant: 10).isActive = true
+        line.setHeightWidth(width: ScreenSize.SCREEN_WIDTH - 25, height: 3)
+        
+        painLabel.translatesAutoresizingMaskIntoConstraints = false
+        painLabel.topAnchor.constraint(equalTo: line.bottomAnchor, constant: 10).isActive = true
+        painLabel.centerXAnchor.constraint(equalTo: pain.centerXAnchor).isActive = true
+        
+        
+        
+        standardStyling()
+    }
+    
+    func standardStyling() {
+        date.font = UIFont(name: "HelveticaNeue-bold", size: fontSize.xXXLarge.rawValue)
         date.textColor = .white
-        date.text = "Kash is a bitch"
+        date.text = "March 24th, 2018"
+        
+        painLabel.text = "Pain"
+        painLabel.font = UIFont(name: "HelveticaNeue-bold", size: fontSize.large.rawValue)
+        painLabel.textColor = .white
+        
+        line.backgroundColor = UIColor(hex: "0E253D")
     }
 }
---
+++ CareTake/CareTake/View/PainView.swift
@@ -0,0 +1,51 @@
+//
+//  PainView.swift
+//  CareTake
+//
+//  Created by Dev on 11/18/18.
+//  Copyright © 2018 Wesaturate. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PainView: UIView {
+    var painCount: Int?
+    var painLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    convenience init(pain: Int, frame: CGRect) {
+        self.init(frame: frame)
+        self.painCount = pain
+        buildColor(pain: pain)
+    }
+    
+    func buildColor(pain: Int) {
+        switch pain {
+        case 1,2,3:
+            self.backgroundColor = .white
+            painLabel.textColor = .black
+        case 4,5,6:
+            self.backgroundColor = UIColor(hex: "6271fc", alpha: 1.0)
+            painLabel.textColor = .black
+        case 7,8,9:
+            self.backgroundColor = UIColor(hex: "E91E63", alpha: 0.70)
+            painLabel.textColor = .white
+        default:
+            self.backgroundColor = UIColor(hex: "E91E63", alpha: 0.70)
+            painLabel.textColor = .white
+        }
+        
+        addSubview(painLabel)
+        painLabel.translatesAutoresizingMaskIntoConstraints = false
+        painLabel.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        painLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        painLabel.text = "\(pain)"
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
